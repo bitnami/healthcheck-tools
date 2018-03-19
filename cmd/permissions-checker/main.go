c08dc0ad3616aae97ca0f78c31b723cbc1271860
Carlos Rodriguez Hernandez <crhernandez@bitnami.com>
2018-03-19 18:08:49 +0100

    Include owner and group + Refactor

--- cmd/permissions-checker/main.go
+++ cmd/permissions-checker/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"strings"
 )
@@ -14,16 +12,25 @@ var (
 	VERSION = "devel"
 	// ShowHidden include hidden files or directories in the loops
 	ShowHidden bool
+	// Verbose print everything
+	Verbose bool
 )
 
 func main() {
 	var (
-		installDir string
-		getVersion bool
+		directory   string
+		application string
+		fileDefault string
+		dirDefault  string
+		getVersion  bool
 	)
-	flag.StringVar(&installDir, "install_dir", "/opt/bitnami", "Installation Directory")
+	flag.StringVar(&directory, "dir", "/opt/bitnami", "Starting directory")
+	flag.StringVar(&application, "application", "", "Application")
+	flag.StringVar(&fileDefault, "file_default", "rw-rw-r--", "File default permissions")
+	flag.StringVar(&dirDefault, "dir_default", "rwxrwxr-x", "Directory default permissions")
 	flag.BoolVar(&getVersion, "version", false, "Show current version")
 	flag.BoolVar(&ShowHidden, "show_hidden", false, "Show hidden files and directories")
+	flag.BoolVar(&Verbose, "verbose", false, "Print every file and directory")
 	flag.Parse()
 
 	if getVersion {
@@ -35,32 +42,15 @@ func main() {
 	PERMISSIONS CHECKS
 ==================================================
 Starting checks with these parameters:
-	- Installation directory: %s
+	- Directory to check: %s
+	- Application: %s
+	- Default file permissions: %s
+	- Default dir permissions: %s
 	- Show hidden: %t
+	- Verbose: %t
 ==================================================
+`, directory, application, fileDefault, dirDefault, ShowHidden, Verbose)
 
-`, installDir, ShowHidden)
-
-	files, err := ioutil.ReadDir(installDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-		switch mode := f.Mode(); {
-		case mode.IsRegular():
-			fileName := f.Name()
-			filePerm := mode.Perm().String()
-			if f.Name()[0:1] != "." || ShowHidden {
-				PrintPermissions("", "f", fileName, filePerm)
-			}
-		case mode.IsDir():
-			dirName := f.Name()
-			dirPerm := mode.Perm().String()
-			if f.Name()[0:1] != "." || ShowHidden {
-				PrintPermissions("", "d", dirName, dirPerm)
-				FindRecursive(strings.Join([]string{installDir, dirName}, "/"), "")
-			}
-		}
-	}
+	fmt.Printf("\x1b[34;1m\n-- Checking %s permissions --\n\x1b[0m", strings.Title(application))
+	FindRecursive(directory, "", fileDefault, dirDefault)
 }
--- cmd/permissions-checker/permissionschecks.go
+++ cmd/permissions-checker/permissionschecks.go
@@ -4,36 +4,57 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os/user"
 	"strings"
+	"syscall"
 )
 
-// FindRecursive iterate in the directory showing permissions in a recursive way
-func FindRecursive(currentPath, level string) {
-	files, err := ioutil.ReadDir(currentPath)
+// FindRecursive iterate in a directory (path) showing permissions in a recursive way
+func FindRecursive(path, level, fileDefPermissions, dirDefPermissions string) {
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	level = level + "  "
 	for _, f := range files {
+		name := f.Name()
+		fullPath := strings.Join([]string{path, name}, "/")
+		owner, _ := user.LookupId(fmt.Sprint(f.Sys().(*syscall.Stat_t).Uid))
+		group, _ := user.LookupGroupId(fmt.Sprint(f.Sys().(*syscall.Stat_t).Gid))
 		switch mode := f.Mode(); {
 		case mode.IsRegular():
-			fileName := f.Name()
 			filePerm := mode.Perm().String()
 			if f.Name()[0:1] != "." || ShowHidden {
-				PrintPermissions(level, "f", fileName, filePerm)
+				CheckPermissions(level, "f", name, fullPath, filePerm, fileDefPermissions, dirDefPermissions, owner.Username, group.Name)
 			}
 		case mode.IsDir():
-			dirName := f.Name()
 			dirPerm := mode.Perm().String()
 			if f.Name()[0:1] != "." || ShowHidden {
-				PrintPermissions(level, "d", dirName, dirPerm)
-				FindRecursive(strings.Join([]string{currentPath, dirName}, "/"), level)
+				CheckPermissions(level, "d", name, fullPath, dirPerm, fileDefPermissions, dirDefPermissions, owner.Username, group.Name)
+				FindRecursive(fullPath, level, fileDefPermissions, dirDefPermissions)
 			}
 		}
 	}
 }
 
-// PrintPermissions print permissions
-func PrintPermissions(level, kind, name, permissions string) {
-	fmt.Printf("%s(%s) %s %s\n", level, kind, name, permissions)
+// CheckPermissions check if permissions are expected
+func CheckPermissions(level, kind, name, path, permissions, fileDefPermissions, dirDefPermissions, owner, group string) {
+	var defaultPermissions string
+	if kind == "f" {
+		defaultPermissions = fileDefPermissions
+	} else if kind == "d" {
+		defaultPermissions = dirDefPermissions
+	}
+
+	if strings.Contains(permissions, defaultPermissions) {
+		if Verbose {
+			fmt.Printf("%s(%s) %s %s %s %s\n", level, kind, name, permissions, owner, group)
+		}
+	} else {
+		if !Verbose {
+			fmt.Printf("\x1b[31;1m(%s) %s %s (expected %s) %s %s\n\x1b[0m", kind, path, permissions, defaultPermissions, owner, group)
+		} else {
+			fmt.Printf("\x1b[31;1m%s(%s) %s %s (expected %s) %s %s\n\x1b[0m", level, kind, name, permissions, defaultPermissions, owner, group)
+		}
+	}
 }
