e3aecfe5461b5a6eb73d17663a8784e4e47d281d
Carlos Rodriguez Hernandez <crhernandez@bitnami.com>
2018-03-22 13:39:19 +0100

    Implement @javsalgar suggestions

--- cmd/permissions-checker/main.go
+++ cmd/permissions-checker/main.go
@@ -3,46 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"regexp"
+	"strings"
 )
+
+type defaultPermissions struct {
+	file  string
+	dir   string
+	owner string
+	group string
+}
+
+type searchSettings struct {
+	hidden        bool
+	exclude       string
+	baseDirectory string
+}
 
 var (
 	// VERSION will be overwritten automatically by the build system
 	VERSION = "devel"
-	// ShowHidden include hidden files or directories in the search
-	ShowHidden bool
-	// Verbose print everything
-	Verbose bool
-	// Exclude some files and/or directories (RegExp)
-	Exclude string
-	// DefaultFilePerm are the default default file permissions
-	DefaultFilePerm string
-	// DefaultDirPerm are the default directory permissions
-	DefaultDirPerm string
-	// DefaultOwner is the default owner
-	DefaultOwner string
-	// DefaultGroup is the default group
-	DefaultGroup string
-	// Directory to check
-	Directory string
 )
 
 func main() {
 	var (
-		getVersion bool
+		getVersion  bool
+		defaultPerm defaultPermissions
+		search      searchSettings
+		verbose     bool
 	)
-	flag.StringVar(&Directory, "dir", "/opt/bitnami", "Directory to check")
-	flag.StringVar(&DefaultFilePerm, "file_default", "rw-rw-r--", "Default file permissions")
-	flag.StringVar(&DefaultDirPerm, "dir_default", "rwxrwxr-x", "Default directory permissions")
-	flag.StringVar(&DefaultOwner, "owner", "bitnami", "Default owner")
-	flag.StringVar(&DefaultGroup, "group", "daemon", "Default group")
-	flag.StringVar(&Exclude, "exclude", "(?!.*)", "Files and/or directories to be excluded")
+
+	flag.StringVar(&defaultPerm.file, "file_default", "rw-rw-r--", "Default file permissions")
+	flag.StringVar(&defaultPerm.dir, "dir_default", "rwxrwxr-x", "Default directory permissions")
+	flag.StringVar(&defaultPerm.owner, "owner", "bitnami", "Default owner")
+	flag.StringVar(&defaultPerm.group, "group", "daemon", "Default group")
+	flag.StringVar(&search.baseDirectory, "dir", "/opt/bitnami", "Directory to check")
+	flag.StringVar(&search.exclude, "exclude", "(?!.*)", "Files and/or directories to be excluded")
+	flag.BoolVar(&search.hidden, "hidden", false, "Includes hidden files and directories")
+	flag.BoolVar(&verbose, "verbose", false, "Print every file and directory")
 	flag.BoolVar(&getVersion, "version", false, "Show current version")
-	flag.BoolVar(&ShowHidden, "show_hidden", false, "Show hidden files and directories")
-	flag.BoolVar(&Verbose, "verbose", false, "Print every file and directory")
 	flag.Parse()
 
-	ManageInputs()
+	// Unifies the format eliminating the last "/" if exists
+	search.baseDirectory = strings.TrimSuffix(search.baseDirectory, "/")
+	search.exclude = strings.TrimSuffix(search.exclude, "/")
+
+	// Checks if the default permissions introduced by the user are in the Linux format
+	if m, _ := regexp.MatchString("^[-rwx]{9,10}$", defaultPerm.file); !m {
+		log.Fatalf("file_default should be in the Linux format (i.e. \"rw-rw-r--\")\n")
+		os.Exit(2)
+	} else if m, _ := regexp.MatchString("^[-rwx]{9,10}$", defaultPerm.dir); !m {
+		log.Fatalf("dir_default should be in the Linux format (i.e. \"rw-rw-r--\")\n")
+		os.Exit(2)
+	}
 
 	if getVersion {
 		fmt.Println(VERSION)
@@ -59,11 +74,11 @@ Starting checks with these parameters:
 	- Owner: %s
 	- Group: %s
 	- Exclude: %s
-	- Show hidden: %t
+	- Include hidden: %t
 	- Verbose: %t
 ==================================================
-`, Directory, DefaultFilePerm, DefaultDirPerm, DefaultOwner, DefaultGroup, Exclude, ShowHidden, Verbose)
+`, search.baseDirectory, defaultPerm.file, defaultPerm.dir, defaultPerm.owner, defaultPerm.group, search.exclude, search.hidden, verbose)
 
 	fmt.Printf("\x1b[34;1m\n-- Checking permissions --\n\x1b[0m")
-	FindRecursive(Directory, 0)
+	FindRecursive(search.baseDirectory, defaultPerm, search, verbose, 0)
 }
--- cmd/permissions-checker/permissionschecks.go
+++ cmd/permissions-checker/permissionschecks.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"os/user"
 	"path/filepath"
 	"regexp"
@@ -12,54 +11,80 @@ import (
 	"syscall"
 )
 
-// FindRecursive iterate in a directory (path) showing permissions in a recursive way
-/* path  - where to look for files and directories
- * level - used to print the output in a hierarchical way
+type currentPermissions struct {
+	permissions           string
+	defaultPermissions    string
+	owner                 string
+	group                 string
+	hasCorrectPermissions bool
+	hasCorrectOwner       bool
+	hasCorrectGroup       bool
+}
+
+// FindRecursive iterates in a directory showing permissions in a recursive way
+/* path [string] 											- where to look for files and directories
+ * defaultPerm [defaultPermissions] 	- data regarding default permissions
+ * 	defaultPerm.file [string]					- default permissions for files
+ * 	defaultPerm.dir [string] 					- default permissions for directories
+ * 	defaultPerm.owner [string] 				- default owner
+ * 	defaultPerm.group [string] 				- default group
+ * search [searchSettings] 						- data regarding search options
+ * 	search.hidden [bool] 							- includes hidden files and directories in the search
+ * 	search.exclude [string] 					- files and/or directories to be excluded (regexp)
+ * 	search.baseDirectory [string]			- base directory
+ * level [int]                        - used to print the output in a hierarchical way
  */
-func FindRecursive(path string, level int) {
+func FindRecursive(path string, defaultPerm defaultPermissions, search searchSettings, verbose bool, level int) {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// In verbose mode we are printing the output in a hierarchical way
-	if Verbose {
+	// In verbose mode, we are printing the output in a hierarchical way
+	if verbose {
 		level++
 	}
 
 	for _, f := range files {
 		name := f.Name()
 
-		relativePath := strings.TrimPrefix(strings.Join([]string{path, name}, "/"), Directory)
+		relativePath := strings.TrimPrefix(strings.Join([]string{path, name}, "/"), search.baseDirectory)
 		fullPath := strings.Join([]string{path, name}, "/")
 
-		if m, _ := regexp.MatchString(Exclude, relativePath); m {
-			if Verbose {
+		if m, _ := regexp.MatchString(search.exclude, relativePath); m {
+			if verbose {
 				fmt.Printf("\x1b[33m%sExcluding %s\n\x1b[0m", strings.Repeat(" ", level), name)
 			}
 		} else {
-			if ShowHidden || !strings.HasPrefix(name, ".") {
+			if search.hidden || !strings.HasPrefix(name, ".") {
 				mode := f.Mode()
 				currentOwner, _ := user.LookupId(fmt.Sprint(f.Sys().(*syscall.Stat_t).Uid))
 				currentGroup, _ := user.LookupGroupId(fmt.Sprint(f.Sys().(*syscall.Stat_t).Gid))
-				currentPerm := mode.Perm().String()
-				hasCorrectOwner := currentOwner.Username == DefaultOwner
-				hasCorrectGroup := currentGroup.Name == DefaultGroup
+
+				currentPerm := currentPermissions{
+					permissions:     mode.Perm().String(),
+					owner:           currentOwner.Username,
+					group:           currentGroup.Name,
+					hasCorrectOwner: currentOwner.Username == defaultPerm.owner,
+					hasCorrectGroup: currentGroup.Name == defaultPerm.group,
+				}
 
 				if mode.IsRegular() {
-					hasCorrectPermissions := checkPermissions(currentPerm, DefaultFilePerm)
-					printOutput(level, "f", fullPath, currentPerm, DefaultFilePerm, currentOwner.Username, currentGroup.Name, hasCorrectPermissions, hasCorrectOwner, hasCorrectGroup)
+					currentPerm.defaultPermissions = defaultPerm.file
+					currentPerm.hasCorrectPermissions = checkPermissions(currentPerm.permissions, currentPerm.defaultPermissions)
+					printOutput(level, "f", fullPath, currentPerm, defaultPerm, verbose)
 				} else if mode.IsDir() {
-					hasCorrectPermissions := checkPermissions(currentPerm, DefaultDirPerm)
-					printOutput(level, "d", fullPath, currentPerm, DefaultDirPerm, currentOwner.Username, currentGroup.Name, hasCorrectPermissions, hasCorrectOwner, hasCorrectGroup)
-					FindRecursive(fullPath, level)
+					currentPerm.defaultPermissions = defaultPerm.dir
+					currentPerm.hasCorrectPermissions = checkPermissions(currentPerm.permissions, currentPerm.defaultPermissions)
+					printOutput(level, "d", fullPath, currentPerm, defaultPerm, verbose)
+					FindRecursive(fullPath, defaultPerm, search, verbose, level)
 				}
 			}
 		}
 	}
 }
 
-// checkPermissions return true if the permissions are correct (false in other case)
+// checkPermissions returns true if the permissions are correct (false in another case)
 /* currentPermissions [string] - permissions that the file or directory has (i.e. -rwxrwxr-x)
  * defaultPermissions [string] - permissions that should have (i.e. rwxrwxr-x)
  */
@@ -67,48 +92,39 @@ func checkPermissions(currentPermissions, defaultPermissions string) bool {
 	return strings.Contains(currentPermissions, defaultPermissions)
 }
 
-// printOutput Print the data in different formats according to the situation
-/* level [string] 							- used to print the output in a hierarchical way
- * kind [string] 								- "f" if it is file or "d" in case of a directory
- * name [string] 				  			- receive the full path of the file/directory (if verbose only use the name)
- * currentPermissions [string] 	- file/directory current permissions
- * defaultPermissions [string] 	- file/directory default permissions
- * currentOwner [string] 				- file/directory current owner
- * currentGroup [string] 				- file/directory current group
- * hasCorrectPermissions [bool] - true if the permissions are correct
- * hasCorrectOwner [bool] 			- true if the owner is correct
- * hasCorrectGroup [bool] 			- true if the group is correct
+// printOutput Prints the data in different formats according to the situation
+/* level [string] 														- used to print the output in a hierarchical way
+ * kind [string] 															- "f" if it is file or "d" in case of a directory
+ * fullPath [string] 				  								- file/directory full path (if verbose only use the name)
+ * currentPerm [currentPermissions]						- data to print relate
+ * 	currentPerm.permissions [string] 					- file/directory current permissions
+ * 	currentPerm.defaultPermissions [string] 	- file/directory default permissions
+ * 	currentPerm.owner [string] 								- file/directory current owner
+ * 	currentPerm.group [string] 								- file/directory current group
+ * 	currentPerm.hasCorrectPermissions [bool] 	- true if the permissions are correct
+ * 	currentPerm.hasCorrectOwner [bool] 				- true if the owner is correct
+ * 	currentPerm.hasCorrectGroup [bool] 				- true if the group is correct
+ * defaultPerm [defaultPermissions] 					- data regarding default permissions
+ * 	defaultPerm.file [string]									- default permissions for files
+ * 	defaultPerm.dir [string] 									- default permissions for directories
+ * 	defaultPerm.owner [string] 								- default owner
+ * 	defaultPerm.group [string] 								- default group
  */
-func printOutput(level int, kind, name, currentPermissions, defaultPermissions, currentOwner, currentGroup string, hasCorrectPermissions, hasCorrectOwner, hasCorrectGroup bool) {
-	if Verbose {
-		name = filepath.Base(name)
+func printOutput(level int, kind, fullPath string, currentPerm currentPermissions, defaultPerm defaultPermissions, verbose bool) {
+	if verbose {
+		fullPath = filepath.Base(fullPath)
 	}
+	hierarchy := strings.Repeat(" ", level)
 
-	if hasCorrectPermissions && hasCorrectOwner && hasCorrectGroup { // Everything correct
-		if Verbose {
-			fmt.Printf("%s(%s) %s %s %s %s\n", strings.Repeat(" ", level), kind, name, currentPermissions, currentOwner, currentGroup)
+	if currentPerm.hasCorrectPermissions && currentPerm.hasCorrectOwner && currentPerm.hasCorrectGroup { // Everything correct
+		if verbose {
+			fmt.Printf("%s(%s) %s %s %s %s\n", hierarchy, kind, fullPath, currentPerm.permissions, currentPerm.owner, currentPerm.group)
 		}
-	} else if hasCorrectPermissions && (!hasCorrectOwner || !hasCorrectGroup) { // Permissions correct, fails owner or group
-		fmt.Printf("\x1b[31;1m%s(%s) %s %s %s %s (expected %s %s)\n\x1b[0m", strings.Repeat(" ", level), kind, name, currentPermissions, currentOwner, currentGroup, DefaultOwner, DefaultGroup)
-	} else if !hasCorrectPermissions && (hasCorrectOwner && hasCorrectGroup) { // Permissions wrong, owner and group correct
-		fmt.Printf("\x1b[31;1m%s(%s) %s %s (expected %s) %s %s\n\x1b[0m", strings.Repeat(" ", level), kind, name, currentPermissions, defaultPermissions, currentOwner, currentGroup)
-	} else if !hasCorrectPermissions && (!hasCorrectOwner || !hasCorrectGroup) { // Nothing correct
-		fmt.Printf("\x1b[31;1m%s(%s) %s %s (expected %s) %s %s (expected %s %s)\n\x1b[0m", strings.Repeat(" ", level), kind, name, currentPermissions, defaultPermissions, currentOwner, currentGroup, DefaultOwner, DefaultGroup)
-	}
-}
-
-// ManageInputs Validate and/or format the parameters entered by the user
-func ManageInputs() {
-	// Unify the format eliminating the last / if exists
-	Directory = strings.TrimSuffix(Directory, "/")
-	Exclude = strings.TrimSuffix(Exclude, "/")
-
-	// Check if the default permissions introduced by the user are in the Linux format
-	if m, _ := regexp.MatchString("^[-rwx]{9,10}$", DefaultFilePerm); !m {
-		log.Fatalf("file_default should be in the Linux format (i.e. \"rw-rw-r--\")\n")
-		os.Exit(2)
-	} else if m, _ := regexp.MatchString("^[-rwx]{9,10}$", DefaultDirPerm); !m {
-		log.Fatalf("dir_default should be in the Linux format (i.e. \"rw-rw-r--\")\n")
-		os.Exit(2)
+	} else if currentPerm.hasCorrectPermissions && (!currentPerm.hasCorrectOwner || !currentPerm.hasCorrectGroup) { // Permissions correct, fails owner or group
+		fmt.Printf("\x1b[31;1m%s(%s) %s %s %s %s (expected %s %s)\n\x1b[0m", hierarchy, kind, fullPath, currentPerm.permissions, currentPerm.owner, currentPerm.group, defaultPerm.owner, defaultPerm.group)
+	} else if !currentPerm.hasCorrectPermissions && (currentPerm.hasCorrectOwner && currentPerm.hasCorrectGroup) { // Permissions wrong, owner and group correct
+		fmt.Printf("\x1b[31;1m%s(%s) %s %s (expected %s) %s %s\n\x1b[0m", hierarchy, kind, fullPath, currentPerm.permissions, currentPerm.defaultPermissions, currentPerm.owner, currentPerm.group)
+	} else if !currentPerm.hasCorrectPermissions && (!currentPerm.hasCorrectOwner || !currentPerm.hasCorrectGroup) { // Nothing correct
+		fmt.Printf("\x1b[31;1m%s(%s) %s %s (expected %s) %s %s (expected %s %s)\n\x1b[0m", hierarchy, kind, fullPath, currentPerm.permissions, currentPerm.defaultPermissions, currentPerm.owner, currentPerm.group, defaultPerm.owner, defaultPerm.group)
 	}
 }
